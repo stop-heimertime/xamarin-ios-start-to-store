058aa9f4d4acbc204e08cb5f04688e6355e81e48
matthewheim <zeigheim@gmail.com>
2017-01-28 14:42:21 -0500

    * RaysHotDogs.Core.csproj: final checkin of module 1
    * HotDogDataService.cs:

---
+++ RaysHotDogs.Core/Service/HotDogDataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RaysHotDogs.Core
+{
+	public class HotDogDataService
+	{
+		private static HotDogRepository hotDogRepository = new HotDogRepository();
+
+		public HotDogDataService()
+		{
+		}
+
+		public List<HotDog> GetAllHotDogs()
+		{
+			return hotDogRepository.GetAllHotDogs();
+		}
+
+		public List<HotDogGroup> GetGroupedHotDogs()
+		{
+			return hotDogRepository.GetGroupedHotDogs();
+		}
+
+		public List<HotDog> GetHotDogsForGroup(int hotDogGroupId)
+		{
+			return hotDogRepository.GetHotDogsForGroup(hotDogGroupId);
+		}
+	}
+}
